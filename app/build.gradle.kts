cad972d8c4df67ffa8a0e9da764cca4c1b6a396e
NikolaDX <nikolar2903@gmail.com>
2025-10-02 19:29:01 +0200

    Began with authentication

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
